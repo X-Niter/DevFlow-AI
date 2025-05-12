aac7de360c0a5ad76e2ae27ed51817854351ea72
X-Niter <rileyman1999@gmail.com>
2025-05-12 00:56:51 -0500

    Continued improving devflow

--- dashboard/src/pages/_app.tsx
+++ dashboard/src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import { Toaster } from 'react-hot-toast'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
