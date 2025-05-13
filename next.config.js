dea624383b29649154419dd588f71e90098beefc
X-Niter <rileyman1999@gmail.com>
2025-05-13 14:08:26 -0500

    next.js configuration

--- next.config.js
+++ next.config.js
@@ -2,13 +2,10 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
+  output: 'export',
   assetPrefix: isProd ? '/devflow-dashboard/' : '',
   basePath: isProd ? '/devflow-dashboard' : '',
   trailingSlash: true,
-  // disable Next’s new App Router (we’re using pages/ only)
-  experimental: {
-    appDir: false,
-  },
   exportPathMap: async (defaultMap) => ({
       '/landing': defaultMap['/landing'],
       '/admin': defaultMap['/admin'],
